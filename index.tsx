b03445d859dda068b9f39658173d0e6c2c68fd53
laurentdu 47 <ecolomique@aol.com>
2025-08-24 15:45:14 +0100

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,9 @@
+// pages/index.tsx
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Bienvenue sur NatiVIO 🌍</h1>
+      <p>Votre guide local, partout en France.</p>
+    </div>
+  );
+}
